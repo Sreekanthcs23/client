aaed287e87d7a2a51ca3ec6c21b7df64cb277d17
Your Name <you@example.com>
2023-03-23 10:41:52 +0530

    third

--- src/components/Education.jsx
+++ src/components/Education.jsx
@@ -1,6 +1,13 @@
 import React,{useState,useEffect} from "react";
 import Axios from "axios";
 import styles from "./Education.module.css";
+import {HiAcademicCap} from "react-icons/hi2";
+import {HiBuildingLibrary} from "react-icons/hi2";
+import {HiDocumentArrowUp} from "react-icons/hi2";
+import {IoCalendarSharp} from "react-icons/io5";
+import DatePicker from "react-datepicker";
+import "react-datepicker/dist/react-datepicker.css";
+/*import "./CustomDatePicker.css";*/
 
 function Education() {
     const [degree,setDegree] = useState("");
@@ -35,19 +42,27 @@ function Education() {
                 <div className={styles.left}>
                     <div className="form">
                         <h2>Add new row</h2>
-                        <label for="degree">Degree</label>
+                        <label for="degree"><HiAcademicCap/>Degree</label>
                         <input type="text" id="degree" onChange={(e) => {setDegree(e.target.value)}} /><br />
                         <label for="branch">Branch</label>
                         <input type="text" id="branch" onChange={(e) => {setBranch(e.target.value)}} /><br />
                         <label for="specialization">Specialization</label>
                         <input type="text" id="specialization" onChange={(e) => {setSpecialization(e.target.value)}} /><br />
-                        <label for="university">University</label>
+                        <label for="university"><HiBuildingLibrary/>University</label>
                         <input type="text" id="university" onChange={(e) => {setUniversity(e.target.value)}} /><br />
-                        <label for="date">Date of acquiring</label>
-                        <input type="text" id="date" onChange={(e) => {setDate(e.target.value)}} /><br />
+                        <label for="date"><IoCalendarSharp/>Date of acquiring</label>
+                        <DatePicker
+                            className="custom-datepicker"
+                            id="date"
+                            selected={date}
+                            onChange={(date) => setDate(date)}
+                            dateFormat="dd/MM/yyyy"
+                            showYearDropdown
+                            scrollableMonthYearDropdown
+                        />
                         <label for="marks">Marks</label>
                         <input type="text" id="marks" onChange={(e) => {setMarks(e.target.value)}} /><br />
-                        <label for="certificate">Certificate</label>
+                        <label for="certificate"><HiDocumentArrowUp/>Certificate</label>
                         <input type="file" id="certificate" /><br />
                         <button onClick={submitForm}>Add</button>
                     </div>
