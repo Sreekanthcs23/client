f32ce355e056a2d28fb865ebbca0be7cde368a38
Adarshms <msadarshatholi@gmail.com>
2023-05-16 15:48:25 +0530

    .

--- src/components/professional/Professional.jsx
+++ src/components/professional/Professional.jsx
@@ -1,170 +1,270 @@
-import React,{useState,useEffect} from "react";
-import Table from 'react-bootstrap/Table';
+import React, { useState, useEffect } from "react";
+import Table from "react-bootstrap/Table";
 import Axios from "axios";
-import Profrow from "./Profrow";
-import Navbar from "./Navbar";
-import Sidebar from "./Sidebar";
+import Edurow from "./Profrow";
+import Navbar from "../Navbar";
+import Sidebar from "../Sidebar";
 import styles from "./Professional.module.css";
-import {HiDocumentArrowUp} from "react-icons/hi2";
-import {IoCalendarSharp} from "react-icons/io5";
+import { HiAcademicCap } from "react-icons/hi2";
+import { HiBuildingLibrary } from "react-icons/hi2";
+import { HiDocumentArrowUp } from "react-icons/hi2";
+import { IoCalendarSharp } from "react-icons/io5";
 import DatePicker from "react-datepicker";
 import "react-datepicker/dist/react-datepicker.css";
+import Breadcrumbs from "@mui/material/Breadcrumbs";
+import Link from "@mui/material/Link";
+import NavigateNextIcon from "@mui/icons-material/NavigateNext";
+import HomeIcon from "@mui/icons-material/Home";
+import EditIcon from "@mui/icons-material/Edit";
+import AddIcon from "@mui/icons-material/Add";
+import ScoreboardIcon from "@mui/icons-material/Scoreboard";
+import AccountTreeIcon from "@mui/icons-material/AccountTree";
+import AdjustIcon from "@mui/icons-material/Adjust";
+import Button from "@mui/material/Button";
 //import "./CustomDatePicker.css";
 
 function Professional() {
+  const [degree, setDegree] = useState("");
+  const [branch, setBranch] = useState("");
+  const [specialization, setSpecialization] = useState("");
+  const [university, setUniversity] = useState("");
+  const [date, setDate] = useState("");
+  const [marks, setMarks] = useState("");
+  const [data, setData] = useState([]);
 
-  
-        const [joiningdate,setJoiningdate] = useState("");
-        const [joiningdesignation,setJoiningdesignation] = useState("");
-        const [dateofproblemdeclaration,setDateofproblemdeclaration] = useState("");
-        const [promotiondate,setPromotiondate] = useState("");
-        const [promotiondesignation,setPromotiondesignation] = useState("");
-        const [longleaveinfo,setLongleaveinfo] = useState("");
-        const [data,setData] = useState([]);
-        
-        const [isVisible,setIsVisible] = useState(false);
-    
-        function toggleVisibilty() {
-            setIsVisible(!isVisible);
-        }
-    
-        useEffect(() => {
-            try {
-                Axios.get('http://localhost:3001/professional/select').then((response) => {
-                   setData(response.data)
-                    console.log(response.data);
-                });
-            }catch(e)
-            {
-                console.log(e);
-            }
-            
-        },[]);
-    
-        const submitForm = () => {
-            toggleVisibilty();
-            Axios.post('http://localhost:3001/professional/insert',{
-                joiningdate:joiningdate,
-                joiningdesignation:joiningdesignation,
-                dateofproblemdeclaration:dateofproblemdeclaration,
-                promotiondate:promotiondate,
-                promotiondesignation:promotiondesignation,
-                longleaveinfo:longleaveinfo
-            }).then(() => { alert("Submitted") });
-        } 
-    
-        const data1 = [
-            {
-                id:1,
-                joiningdate:"10-12-2000",
-                joiningdesignation:"Assistant Prof",
-                dateofproblemdeclaration:"12-02-2001",
-                promotiondate:"25-05-2005",
-                promotiondesignation:"Prof",
-                longleaveinfo:"No long leave yet",
-                //marks:"9.5"
-            }
-            
-        ]
-    
-        return (
-            <div className={styles.page}>
-                <div className={styles.edu_navbar}>
-                    <Navbar />
-                </div>
-                
-                
-                <div className={styles.edu_parent}>
-                    <div className={styles.edu_left}>
-                        <Sidebar />
-                    </div>
-                    
-                    <div className={styles.edu_right}>
-                        {!isVisible && <div>
-                            <h1 className={styles.title}>PROFESSIONAL EXPERIENCE</h1>
-                             
-                            <h2>Current Position</h2>
-                            <div className={styles.edu_div}>
-                             {data1.map((item => {
-                                return (<Profrow
-                                    id={item.id}
-                                    joiningdate={item.joiningdate.toString().slice(0,10)}
-                                    joiningdesignation={item.joiningdesignation}
-                                    dateofproblemdeclaration={item.dateofproblemdeclaration.toString().slice(0,10)}
-                                    promotiondate={item.promotiondate.toString().slice(0,10)}
-                                    promotiondesignation={item.promotiondesignation}
-                                    longleaveinfo={item.longleaveinfo}
-                                    >
-                                 </Profrow>)
-                                }))}
-                            </div>
-                        </div> }
+  const [isVisible, setIsVisible] = useState(false);
 
-                        <button className={styles.submit_button} onClick={toggleVisibilty}>Change Details</button>
-    
-                            { isVisible &&  <div className={styles.edu_form}>
-                        
-                        <div className={styles.form}>
-                            <h1>Update details</h1>
+  function toggleVisibilty() {
+    setIsVisible(!isVisible);
+  }
 
-                            <label for="joiningdate"><IoCalendarSharp/>Joining Date</label>
-                            <DatePicker
-                                className={styles.date_input}
-                                id="joiningdate"
-                                selected={joiningdate}
-                                onChange={(joiningdate) => setJoiningdate(joiningdate)}
-                                dateFormat="dd/mm/yyyy"
-                                showYearDropdown
-                                scrollableMonthYearDropdown
-                            />
+  useEffect(() => {
+    try {
+      Axios.get("http://localhost:3001/education/select").then((response) => {
+        setData(response.data);
+        console.log(response.data);
+      });
+    } catch (e) {
+      console.log(e);
+    }
+  }, []);
 
-                            <label for="joiningdesignation">Joining Designation</label>
-                            <input type="text" id="joiningdesignation" onChange={(e) => {setJoiningdesignation(e.target.value)}} /><br />
-                            
-                            <label for="dateofproblemdeclaration"><IoCalendarSharp/>Date of Problem Declaration</label>
-                            <DatePicker
-                                className={styles.date_input}
-                                id="dateofproblemdeclaration"
-                                selected={dateofproblemdeclaration}
-                                onChange={(dateofproblemdeclaration) => setDateofproblemdeclaration(dateofproblemdeclaration)}
-                                dateFormat="dd/mm/yyyy"
-                                showYearDropdown
-                                scrollableMonthYearDropdown
-                            />
+  const submitForm = () => {
+    toggleVisibilty();
+    Axios.post("http://localhost:3001/education/insert", {
+      degree: degree,
+      branch: branch,
+      specialization: specialization,
+      university: university,
+      date: date,
+      marks: marks,
+    }).then(() => {
+      alert("submitted");
+    });
+  };
 
-                            <label for="promotiondate"><IoCalendarSharp/>Promotion Date</label>
-                            <DatePicker
-                                className={styles.date_input}
-                                id="promotiondate"
-                                selected={promotiondate}
-                                onChange={(promotiondate) => setPromotiondate(promotiondate)}
-                                dateFormat="dd/MM/yyyy"
-                                showYearDropdown
-                                scrollableMonthYearDropdown
-                            />
+  const data1 = [
+    {
+      id: 1,
+      degree: "B-Tech",
+      branch: "CSE",
+      specialization: "Machine learninng",
+      university: "IIT",
+      college: "IIT Bombay",
+      date_of_acq: "16-08-2020",
+      marks: "9.5",
+    },
+    {
+      id: 2,
+      degree: "M-Tech",
+      branch: "CSE",
+      specialization: "Machine learninng",
+      university: "IIT",
+      college: "IIT Bombay",
+      date_of_acq: "16-08-2020",
+      marks: "9.5",
+    },
+    {
+      id: 3,
+      degree: "Phd",
+      branch: "CSE",
+      specialization: "Machine learninng",
+      university: "IIT",
+      college: "IIT Bombay",
+      date_of_acq: "16-08-2020",
+      marks: "9.5",
+    },
+  ];
 
-                            <label for="promotiondesignation">Promotion Designation</label>
-                            <input type="text" id="promotiondesignation" onChange={(e) => {setPromotiondesignation(e.target.value)}} /><br />
-                            
-                            <label for="longleaveinfo">Long Leave Info</label>
-                            <input type="text" id="longleaveinfo" onChange={(e) => {setLongleaveinfo(e.target.value)}} /><br />
-                            
-                         
-                            <label for="certificate"><HiDocumentArrowUp/>Certificate</label>
-                            <input type="file" id="certificate" /><br />
-                            <button onClick={submitForm}>Add</button>
-                        </div>
-                    </div> }
-                    </div>
-    
-                    
-                    
-                    
-                </div>
-                
+  return (
+    <div className={styles.edu_page}>
+      <div className={styles.edu_navbar}>
+        <Navbar />
+      </div>
+
+      <div className={styles.edu_sidebar}>
+        <Sidebar />
+      </div>
+
+      <div className={styles.edu_content}>
+        <Breadcrumbs
+          separator={<NavigateNextIcon fontSize="small" />}
+          aria-label="breadcrumb"
+        >
+          <Link
+            underline="hover"
+            sx={{ display: "flex", alignItems: "center" }}
+            color="inherit"
+            href="/home"
+          >
+            <HomeIcon sx={{ mr: 0.5 }} fontSize="inherit" />
+            Home
+          </Link>
+          <Link
+            underline="hover"
+            sx={{ display: "flex", alignItems: "center" }}
+            color="inherit"
+            href="/research/consultancy"
+          >
+            Education
+          </Link>
+        </Breadcrumbs>
+
+        {!isVisible && (
+          <div className={styles.edu_div}>
+            <Button
+              variant="outlined"
+              startIcon={<EditIcon />}
+              onClick={toggleVisibilty}
+            >
+              UPDATE
+            </Button>
+            <div className={styles.edu_list}>
+              {data1.map((item) => {
+                return (
+                  <Edurow
+                    key={item.id}
+                    degree={item.degree}
+                    branch={item.branch}
+                    specialization={item.specialization}
+                    university={item.university}
+                    college={item.college}
+                    date={item.date_of_acq.toString().slice(0, 10)}
+                    marks={item.marks}
+                  ></Edurow>
+                );
+              })}
             </div>
-        )
-   
+          </div>
+        )}
+
+        {isVisible && (
+          <div className={styles.edu_form}>
+            <h1 className={styles.edu_form_title}>Update details</h1>
+            <div className={styles.edu_form_left}>
+              <label for="degree">
+                <HiAcademicCap />
+                Degree
+              </label>
+              <br />
+              <input
+                type="text"
+                id="degree"
+                onChange={(e) => {
+                  setDegree(e.target.value);
+                }}
+              />
+              <br />
+              <label for="branch">
+                <AccountTreeIcon fontSize="small" /> Branch
+              </label>
+              <br />
+              <input
+                type="text"
+                id="branch"
+                onChange={(e) => {
+                  setBranch(e.target.value);
+                }}
+              />
+              <br />
+              <label for="specialization">
+                <AdjustIcon fontSize="small" /> Specialization
+              </label>
+              <br />
+              <input
+                type="text"
+                id="specialization"
+                onChange={(e) => {
+                  setSpecialization(e.target.value);
+                }}
+              />
+              <br />
+            </div>
+            <div className={styles.edu_form_right}>
+              <label for="university">
+                <HiBuildingLibrary />
+                University
+              </label>
+              <br />
+              <input
+                type="text"
+                id="university"
+                onChange={(e) => {
+                  setUniversity(e.target.value);
+                }}
+              />
+              <br />
+              <label for="date">
+                <IoCalendarSharp />
+                Date of acquiring
+              </label>
+              <br />
+              <DatePicker
+                className={styles.date_input}
+                id="date"
+                selected={date}
+                onChange={(date) => setDate(date)}
+                dateFormat="dd/MM/yyyy"
+                showYearDropdown
+                scrollableMonthYearDropdown
+              />
+              <br />
+              <label for="marks">
+                <ScoreboardIcon fontSize="small" />
+                Marks
+              </label>
+              <br />
+              <input
+                type="text"
+                id="marks"
+                onChange={(e) => {
+                  setMarks(e.target.value);
+                }}
+              />
+              <br />
+              <label for="certificate">
+                <HiDocumentArrowUp />
+                Certificate
+              </label>
+              <br />
+              <input type="file" id="certificate" />
+            </div>
+            <br />
+            <div className={styles.edu_form_button}>
+              <Button
+                variant="contained"
+                color="success"
+                startIcon={<AddIcon />}
+                onClick={submitForm}
+              >
+                ADD
+              </Button>
+            </div>
+          </div>
+        )}
+      </div>
+    </div>
+  );
 }
 
-export default Professional;
+export default Education;
--- src/components/professional/Profrow.jsx
+++ src/components/professional/Profrow.jsx
@@ -1,35 +1,48 @@
 import styles from "./Profrow.module.css";
+import CardMembershipIcon from "@mui/icons-material/CardMembership";
+import Button from "@mui/material/Button";
 
-function Profrow(props) {
-    return (
-        <div className={styles.main_div}>
-            <h1>{props.degree}</h1>
-            <div className={styles.col_div}>
-                <div>
-                    <label>Joining Date</label>
-                    <input type="date" disabled="disabled" value={props.joiningdate} /><br />
-                    <label>Joining Designation</label>
-                    <input type="text" disabled="disabled" value={props.joiningdesignation} /><br />
-                </div>
-                <div>
-                    <label>Date of Problem Declaration</label>
-                    <input type="date" disabled="disabled" value={props.dateofproblemdeclaration} /><br />
-                    <label>Promotion Date</label>
-                    <input type="text" disabled="disabled" value={props.promotiondate} /><br />
-                </div>
-                <div>
-                    <label>Promotion Designation</label>
-                    <input type="text" disabled="disabled" value={props.promotiondesignation} /><br />
-                    <label>Long leave info</label>
-                    <input type="text" disabled="disabled" value={props.longleaveinfo} /><br />
-                </div>
-                <div>
-                    <button type="button" >Certificate</button>
-                </div>
-            </div>
-            
+function Edurow(props) {
+  return (
+    <div className={styles.main_div}>
+      <h1>{props.degree}</h1>
+      <div className={styles.col_div}>
+        <div>
+          <label>Branch</label>
+          <input type="text" disabled="disabled" value={props.branch} />
+          <br />
+          <label>Specialization</label>
+          <input type="text" disabled="disabled" value={props.specialization} />
+          <br />
         </div>
-    )
+        <div>
+          <label>University</label>
+          <input type="text" disabled="disabled" value={props.university} />
+          <br />
+          <label>College</label>
+          <input type="text" disabled="disabled" value={props.college} />
+          <br />
+        </div>
+        <div>
+          <label>Date of acquiry</label>
+          <input type="text" disabled="disabled" value={props.date} />
+          <br />
+          <label>Marks</label>
+          <input type="text" disabled="disabled" value={props.marks} />
+          <br />
+        </div>
+        <div>
+          <Button
+            variant="contained"
+            startIcon={<CardMembershipIcon />}
+            size="small"
+          >
+            Cerificate
+          </Button>
+        </div>
+      </div>
+    </div>
+  );
 }
 
-export default Profrow;
+export default Edurow;
